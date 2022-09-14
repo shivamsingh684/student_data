95fbfdcf02266ee89819aa53f307af9e806e0ff4
Shaily-Tripathi <tripathishaily95@gmail.com>
2022-09-14 11:20:40 +0530

    Boiler Plate Complete

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://plutonium-co:Y7XVLNecywgcT8ky@cluster0.ognlwhp.mongodb.net/group58Database", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
---
+++ src/route/route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const collegeController = require("../controllers/collegeController")
+const internController = require("../controllers/internController")
+
+router.post("/functionup/colleges", collegeController.college)
+
+router.post("/functionup/interns", internController.intern)
+
+router.get("/functionup/collegeDetails", collegeController.collegeDetails)
+
+
+module.exports = router;
