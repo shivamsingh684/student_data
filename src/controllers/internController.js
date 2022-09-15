8e01a894822894e056e85041db6eec097bffeb01
Shaily-Tripathi <tripathishaily95@gmail.com>
2022-09-15 16:26:25 +0530

    edge case

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,4 +1,5 @@
 const internModel = require("../models/internModel");
+const collegeModel = require("../models/collegeModel")
 const mongoose = require("mongoose");
 
 // =============================Create Intern Api==============================
@@ -6,6 +7,9 @@ const mongoose = require("mongoose");
 const createIntern = async function (req, res) {
   try {
     const internData = req.body;
+    const clgId = internData.collegeId
+    const clgData = await collegeModel.findById(clgId)
+    if(!clgData) {return res.status(404).send({status:false, message: "collegeId not found"});}
     
     const savedInternData =  await internModel.create(internData);
     return res.status(201).send({status:true, data: savedInternData});
--- src/validation/validation.js
+++ src/validation/validation.js
@@ -31,7 +31,7 @@ const collegeValidation=async function(req,res,next){
         if(!fullName){return res.status(400).send({status:false,msg:"please provide the fullName"})}
         if(typeof fullName==='string' && fullName.trim().length === 0){return res.status(400).send({status:false,msg:"fullName is empty"})}
         if(typeof fullName !== 'string') {return res.status(400).send({status:false,msg:"fullName should be string"})}
-        if (!(/^[a-zA-Z]{1,}/).test(fullName)) return res.status(400).send({ status: false, msg: " Please enter fullName as A-Z or a-z" })
+        if (!(/^[a-zA-Z ]{1,}/).test(fullName)) return res.status(400).send({ status: false, msg: " Please enter fullName as A-Z or a-z" })
 
         if(!logoLink){return res.status(400).send({status:false,msg:"please provide the logoLink"})}
         if(typeof logoLink==='string' && logoLink.trim().length === 0){return res.status(400).send({status:false,msg:"logoLink is empty"})}
@@ -69,6 +69,7 @@ const collegeValidation=async function(req,res,next){
         if(typeof name !== 'string') {return res.status(400).send({status:false,msg:"name should be string"})}
         if (!(/^[a-zA-Z ]{2,30}$/).test(name)) return res.status(400).send({ status: false, msg: " Please enter name as A-Z or a-z" })
 
+        if(!email){return res.status(400).send({status:false,msg:"please provide the email"})}
         if(typeof email==='string' && email.trim().length === 0){return res.status(400).send({status:false,msg:"email is empty"})}
         if(typeof email !== 'string') {return res.status(400).send({status:false,msg:"email should be string"})}
         if (!(/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/).test(email.trim())) { return res.status(400).send({ status: false, msg: "please provide valid email" }) }
@@ -76,6 +77,7 @@ const collegeValidation=async function(req,res,next){
         let intern = await internModel.findOne({ email: email })
         if (intern) { return res.status(400).send({ status: false, msg: "this email already exists please provide another email" }) }
         
+        if(!mobile){return res.status(400).send({status:false,msg:"please provide the mobile"})}
         if(typeof mobile==='string' && mobile.trim().length === 0){return res.status(400).send({status:false,msg:"mobile number is empty"})}
         if(typeof mobile !== 'string') {return res.status(400).send({status:false,msg:"mobile number should be string"})}
         if (!(/^[0]?[6789]\d{9}$/).test(mobile.trim())) { return res.status(400).send({ status: false, msg: "please provide valid mobile number" }) }
